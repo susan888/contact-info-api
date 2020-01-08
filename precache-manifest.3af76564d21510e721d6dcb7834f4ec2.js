a98e9f3bd4f58e0a72cb9713fa4faa83812ceeb9
Susan Duwal <duwalsusan8@gmail.com>
2020-01-08 13:42:40 +0545

    Updates

---
+++ precache-manifest.3af76564d21510e721d6dcb7834f4ec2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822b96ae62588b14ebb52d6d69d6e618",
+    "url": "/react-user-profile/index.html"
+  },
+  {
+    "revision": "49c6321ee3edd16e322c",
+    "url": "/react-user-profile/static/css/2.66ffbf77.chunk.css"
+  },
+  {
+    "revision": "4fb422ddbfe8f837fe6e",
+    "url": "/react-user-profile/static/css/main.26e606d0.chunk.css"
+  },
+  {
+    "revision": "49c6321ee3edd16e322c",
+    "url": "/react-user-profile/static/js/2.1651f1bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-user-profile/static/js/2.1651f1bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fb422ddbfe8f837fe6e",
+    "url": "/react-user-profile/static/js/main.180d25bc.chunk.js"
+  },
+  {
+    "revision": "3989a68aafa780b16033",
+    "url": "/react-user-profile/static/js/runtime-main.878e4366.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-user-profile/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-user-profile/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-user-profile/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-user-profile/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-user-profile/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6b0022312d41080436c52da571d5c697",
+    "url": "/react-user-profile/static/media/profileImage.6b002231.png"
+  }
+]);
